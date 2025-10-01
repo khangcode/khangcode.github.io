cc4b57be077c10464ef35c577282129b5cc7ab91
khang <khang.nguyen@live.com>
2025-10-01 19:36:01 +0700

    Fresh start

---
+++ searchindex.en.js
@@ -0,0 +1,74 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "LOTTE Finance Legal",
+    "content": "This is a new chapter of Internal Regulations.",
+    "description": "This is a new chapter of Internal Regulations.",
+    "tags": [],
+    "title": "Internal Regulations",
+    "uri": "/internal_regulations/index.html"
+  },
+  {
+    "breadcrumb": "LOTTE Finance Legal",
+    "content": "This is a new chapter of contract.\nĐiều khoản tiêu chuẩn Cam đoan và bảo đảm Bất khả kháng Phạt vi phạm và bồi thường thiệt hại Thời hạn hiệu lực Giải quyết tranh chấp Các loại Hợp đồng Hợp đồng mua bán hàng hóa Hợp đồng dịch vụ Hợp đồng tín dụng Hợp đồng thế chấp Hợp đồng tiền gửi Hợp đồng phần mềm Mẫu Hợp đồng Mẫu Thỏa thuận bảo mật (NDA) Mẫu Hợp đồng cho vay Mẫu Hợp đồng thế chấp động sản Mẫu Hợp đồng thế chấp bất động sản",
+    "description": "This is a new chapter of contract.\nĐiều khoản tiêu chuẩn Cam đoan và bảo đảm Bất khả kháng Phạt vi phạm và bồi thường thiệt hại Thời hạn hiệu lực Giải quyết tranh chấp Các loại Hợp đồng Hợp đồng mua bán hàng hóa Hợp đồng dịch vụ Hợp đồng tín dụng Hợp đồng thế chấp Hợp đồng tiền gửi Hợp đồng phần mềm Mẫu Hợp đồng Mẫu Thỏa thuận bảo mật (NDA) Mẫu Hợp đồng cho vay Mẫu Hợp đồng thế chấp động sản Mẫu Hợp đồng thế chấp bất động sản",
+    "tags": [],
+    "title": "Contracts",
+    "uri": "/contracts/index.html"
+  },
+  {
+    "breadcrumb": "LOTTE Finance Legal",
+    "content": "This is a new chapter.",
+    "description": "This is a new chapter.",
+    "tags": [],
+    "title": "Legal Advices",
+    "uri": "/legal-advices/index.html"
+  },
+  {
+    "breadcrumb": "LOTTE Finance Legal",
+    "content": "This is a new chapter.\nCác văn bản pháp luật thông dụng\nChung Luật các tổ chức tín dụng 2024 tvpl\rLuật doanh nghiệp 2020 tvpl\rBộ luật dân sự 2015 tvpl\rCho vay Thông tư Thẻ tín dụng Thông tư 18/2024/TT-NHNN về hoạt động thẻ ngân hàng pdf\rtvpl\rGiao dịch bảo đảm Nguồn vốn",
+    "description": "This is a new chapter.\nCác văn bản pháp luật thông dụng\nChung Luật các tổ chức tín dụng 2024 tvpl\rLuật doanh nghiệp 2020 tvpl\rBộ luật dân sự 2015 tvpl\rCho vay Thông tư Thẻ tín dụng Thông tư 18/2024/TT-NHNN về hoạt động thẻ ngân hàng pdf\rtvpl\rGiao dịch bảo đảm Nguồn vốn",
+    "tags": [],
+    "title": "Legal Docs",
+    "uri": "/legal-docs/index.html"
+  },
+  {
+    "breadcrumb": "LOTTE Finance Legal",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "Phòng Pháp chế LOTTE Finance là bộ phận chịu trách nhiệm tư vấn pháp lý, rà soát pháp lý các văn bản nội bộ, hợp đồng, hỗ trợ xử lý tranh chấp, bảo vệ quyền lợi pháp lý và giảm thiểu rủi ro pháp lý trong hoạt động kinh doanh.\nPhòng Pháp chế LOTTE Finance còn đóng vai trò kiến tạo môi trường, cơ chế, giải pháp, phương án thực hiện tối ưu. Phòng Pháp chế chủ động nghiên cứu, đề xuất các cải tiến, xây dựng quy trình và khung pháp lý phù hợp với thực tiễn kinh doanh, góp phần thúc đẩy sự đổi mới.\nVới đội ngũ chuyên gia giàu kinh nghiệm và cách tiếp cận linh hoạt, tinh thần đồng hành, sáng tạo, hoạt động của Phòng Pháp chế không chỉ nhằm đúng luật mà còn giúp cho Đơn vị tận dụng cơ hội từ các quy định pháp luật để phát triển kinh doanh.\nPhòng Pháp chế đóng vai trò quan trọng trong việc xây dựng nền tảng pháp lý vững chắc, góp phần vào sự phát triển bền vững của Công ty.\nQuy định nội bộ Công việc rà soát quy định nội bộ của Phòng Pháp chế để các quy định, chính sách trong Công ty phù hợp với pháp luật hiện hành, đồng thời đáp ứng thực tiễn hoạt động.\nPhòng Pháp chế kiểm tra tính hợp pháp, tính đồng bộ và khả năng áp dụng của các văn bản nội bộ, phát hiện và khắc phục những điểm chưa chặt chẽ hoặc tiềm ẩn rủi ro. Qua đó, Phòng Pháp chế không chỉ giúp duy trì sự tuân thủ pháp luật mà còn góp phần xây dựng hệ thống quản trị nội bộ hiệu quả, minh bạch và bền vững.\nHợp đồng Công việc rà soát hợp đồng của Phòng Pháp chế là bước quan trọng nhằm đảm bảo tính pháp lý, chặt chẽ và minh bạch trong các thỏa thuận của công ty.\nPhòng chịu trách nhiệm kiểm tra nội dung hợp đồng, phát hiện và xử lý các rủi ro pháp lý tiềm ẩn, đồng thời đề xuất điều chỉnh phù hợp để bảo vệ quyền lợi tối đa cho công ty. Quá trình này không chỉ đảm bảo tuân thủ pháp luật mà còn giúp tối ưu hóa các điều khoản hợp tác, giảm thiểu tranh chấp và xây dựng nền tảng pháp lý vững chắc trong quan hệ đối tác.\nTư vấn Công việc tư vấn pháp luật của Phòng Pháp chế là hỗ trợ các phòng ban và lãnh đạo trong việc giải quyết các vấn đề pháp lý phát sinh trong hoạt động kinh doanh.\nPhòng cung cấp ý kiến tư vấn rõ ràng, chính xác và thực tiễn về các quy định pháp luật, hợp đồng, tranh chấp và các vấn đề liên quan. Đồng thời, Phòng Pháp chế giúp định hướng các quyết định quan trọng, giảm thiểu rủi ro pháp lý và tận dụng cơ hội từ chính sách pháp luật để hỗ trợ công ty phát triển một cách bền vững và an toàn.\nChính sách Công việc góp ý xây dựng pháp luật của Phòng Pháp chế nhằm đảm bảo các quy định pháp luật phù hợp với thực tiễn kinh doanh và lợi ích của doanh nghiệp.\nPhòng tham gia đóng góp ý kiến vào dự thảo văn bản quy phạm pháp luật, phản ánh những khó khăn, vướng mắc từ thực tế áp dụng, đồng thời đề xuất các giải pháp hoàn thiện.\nThông qua việc này, Phòng Pháp chế không chỉ bảo vệ quyền lợi của công ty mà còn đóng góp tích cực vào quá trình xây dựng môi trường pháp lý minh bạch, hiệu quả và công bằng.",
+    "description": "Phòng Pháp chế LOTTE Finance là bộ phận chịu trách nhiệm tư vấn pháp lý, rà soát pháp lý các văn bản nội bộ, hợp đồng, hỗ trợ xử lý tranh chấp, bảo vệ quyền lợi pháp lý và giảm thiểu rủi ro pháp lý trong hoạt động kinh doanh.\nPhòng Pháp chế LOTTE Finance còn đóng vai trò kiến tạo môi trường, cơ chế, giải pháp, phương án thực hiện tối ưu. Phòng Pháp chế chủ động nghiên cứu, đề xuất các cải tiến, xây dựng quy trình và khung pháp lý phù hợp với thực tiễn kinh doanh, góp phần thúc đẩy sự đổi mới.",
+    "tags": [],
+    "title": "LOTTE Finance Legal",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "LOTTE Finance Legal",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  },
+  {
+    "breadcrumb": "LOTTE Finance Legal \u003e Legal Docs",
+    "content": "UBND TP HCM đề xuất, với những người nghỉ hưu trước tuổi, ngoài chính sách hưởng theo Điều 5 Nghị định 29 sẽ được trợ cấp 3 tháng lương cho mỗi năm nghỉ hưu trước tuổi; 5 tháng lương cho 20 năm đầu công tác.\nTừ năm thứ 21 trở đi, cứ mỗi năm công tác người lao động nhận thêm nửa tháng lương. Cán bộ, công chức cấp xã phải nghỉ việc do sắp xếp đơn vị hành chính ngoài chế độ theo khoản 1 Điều 8 Nghị định 29, UBND HCM đề xuất trợ cấp thêm 1,5 tháng lương cho mỗi năm nghỉ hưu trước tuổi, 5 tháng lương cho 20 năm đầu công tác; từ năm thứ 21 trở đi, cứ mỗi năm công tác được cấp thêm nửa tháng lương.\nCán bộ nữ cấp xã nghỉ do sắp xếp đơn vị hành chính ngoài chế độ theo khoản 2, Điều 8 Nghị định 29, sẽ được trợ cấp thêm 5 tháng lương và 1,5 tháng lương cho mỗi năm nghỉ hưu trước tuổi. Những cán bộ thôi việc ngay được trợ cấp thêm 3 tháng lương để tìm việc làm, 1,5 tháng lương cho mỗi năm công tác. Người nghỉ hưu trước tuổi do không đủ điều kiện tuổi tái cử, tái bổ nhiệm được trợ cấp thêm 3 tháng lương cho mỗi năm nghỉ hưu trước tuổi, 5 tháng lương cho 20 năm đầu công tác; từ năm thứ 21 trở đi, mỗi năm trợ cấp thêm nửa tháng lương. Hỗ trợ này cũng áp dụng cho người nghỉ hưu trước tuổi do nguyên nhân bất khả kháng, sức khỏe giảm sút.",
+    "description": "UBND TP HCM đề xuất, với những người nghỉ hưu trước tuổi, ngoài chính sách hưởng theo Điều 5 Nghị định 29 sẽ được trợ cấp 3 tháng lương cho mỗi năm nghỉ hưu trước tuổi; 5 tháng lương cho 20 năm đầu công tác.\nTừ năm thứ 21 trở đi, cứ mỗi năm công tác người lao động nhận thêm nửa tháng lương. Cán bộ, công chức cấp xã phải nghỉ việc do sắp xếp đơn vị hành chính ngoài chế độ theo khoản 1 Điều 8 Nghị định 29, UBND HCM đề xuất trợ cấp thêm 1,5 tháng lương cho mỗi năm nghỉ hưu trước tuổi, 5 tháng lương cho 20 năm đầu công tác; từ năm thứ 21 trở đi, cứ mỗi năm công tác được cấp thêm nửa tháng lương.",
+    "tags": [],
+    "title": "Test Image",
+    "uri": "/legal-docs/test/index.html"
+  },
+  {
+    "breadcrumb": "LOTTE Finance Legal \u003e Legal Docs",
+    "content": "Test mermaid:\ngraph LR;\rA[Hard edge] --\u003e|Link text| B(Round edge)\rB --\u003e C{Decision}\rC --\u003e|One| D[Result one]\rC --\u003e|Two| E[Result two]\rsequenceDiagram\rparticipant Alice\rparticipant Bob\rAlice-\u003e\u003eJohn: Hello John, how are you?\rloop Healthcheck\rJohn-\u003eJohn: Fight against hypochondria\rend\rNote right of John: Rational thoughts \u003cbr/\u003eprevail...\rJohn--\u003eAlice: Great!\rJohn-\u003eBob: How about you?\rBob--\u003eJohn: Jolly good!\rmindmap\rroot((mindmap))\rOrigins\rLong history\r::icon(fa fa-book)\rPopularisation\rBritish popular psychology author Tony Buzan\rResearch\rOn effectiveness\u003cbr/\u003eand features\rOn Automatic creation\rUses\rCreative techniques\rStrategic planning\rArgument mapping\rTools\rPen and paper\rMermaid\rDung Hugo Blox\n- Root\r- Branch 1\r- Sub-branch 1\r- Sub-branch 2\r- Branch 2\r- Sub-branch 1 - Mindmaps\r- Links\r- [Hugo Blox Docs](https://hugoblox.com/docs/)\r- [Discord Community](https://discord.gg/z8wNYzb)\r- [GitHub](https://github.com/HugoBlox/hugo-blox-builder)\r- Features\r- Markdown formatting\r- **inline** ~~text~~ *styles*\r- multiline\rtext\r- `inline code`\r-\r```js\rconsole.log('hello');\rconsole.log('code block');\r```\r- Math: $x = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}$ - Mindmaps\r- Links\r- [Hugo Blox Docs](https://docs.hugoblox.com/)\r- [Discord Community](https://discord.gg/z8wNYzb)\r- [GitHub](https://github.com/HugoBlox/hugo-blox-builder)\r- Features\r- Markdown formatting\r- **inline** ~~text~~ *styles*\r- multiline\rtext\r- `inline code`\r-\r```js\rconsole.log('hello');\rconsole.log('code block');\r```\r- Math: $x = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}$",
+    "description": "Test mermaid:\ngraph LR;\rA[Hard edge] --\u003e|Link text| B(Round edge)\rB --\u003e C{Decision}\rC --\u003e|One| D[Result one]\rC --\u003e|Two| E[Result two]\rsequenceDiagram\rparticipant Alice\rparticipant Bob\rAlice-\u003e\u003eJohn: Hello John, how are you?\rloop Healthcheck\rJohn-\u003eJohn: Fight against hypochondria\rend\rNote right of John: Rational thoughts \u003cbr/\u003eprevail...\rJohn--\u003eAlice: Great!\rJohn-\u003eBob: How about you?\rBob--\u003eJohn: Jolly good!\rmindmap\rroot((mindmap))\rOrigins\rLong history\r::icon(fa fa-book)\rPopularisation\rBritish popular psychology author Tony Buzan\rResearch\rOn effectiveness\u003cbr/\u003eand features\rOn Automatic creation\rUses\rCreative techniques\rStrategic planning\rArgument mapping\rTools\rPen and paper\rMermaid\rDung Hugo Blox",
+    "tags": [],
+    "title": "Test mermaid",
+    "uri": "/legal-docs/mermaid/index.html"
+  }
+]
